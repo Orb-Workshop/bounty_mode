c83dacb48dd3f68b1b99306ee05b3e541ee527ea
Benjamin Zaporzan <benzaporzan@gmail.com>
2026-01-14 11:08:29 -0500

    Added logic relay breakouts

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import {
 
 import { BountySystem, BulletinBoard } from "./index";
 
-
 // Register our Bounty System
 Base.Mount.Register("Bounty", new BountySystem({
+    // Used to hand out money to players, entity 'game_money'
     game_money_targetname: "bounty_mode.money",
 }));
 
 // Spawn T and CT Bulletin Board Actors, which listen to our BountySystem
+// Prefab: `maps/prefabs/bounty_mode/bounty_mode_bulletin_board.vmap`
 const bulletin_t = new BulletinBoard({
     target_name: "bounty_mode.bulletin_board_t",
     team_number: 2,
@@ -23,8 +24,8 @@ const bulletin_ct = new BulletinBoard({
 });
 
 // Debugging, echo event messages.
-//const echo = new Base.MessageTask((key, data) => {
-//    CSS.Msg(`${key} - ${JSON.stringify(data)}`);
+//const echo = new Base.MessageTask((tag, data) => {
+//    CSS.Msg(`${tag} - ${JSON.stringify(data)}`);
 //});
 
 
@@ -34,8 +35,8 @@ function RewardMessage(killer_name: string, death_name: string, reward_amount: n
 }
 
 // Listen for "BountyRewarded" event from the running BountySystem instance.
-const rewardMessage = new Base.MessageTask((key, data) => {
-    if (key !== BountySystem.MessageTag) return;
+const rewardMessage = new Base.MessageTask((tag, data) => {
+    if (tag !== BountySystem.MessageTag) return;
     const { event_name, event_data } = data;
     if (event_name !== "BountyRewarded") return;
     const { player_rewarded, player_killed, reward_amount } = event_data;
@@ -45,6 +46,30 @@ const rewardMessage = new Base.MessageTask((key, data) => {
     CSS.ServerCommand(`say ${reward_message}`);
 });
 
+// Trigger 'logic_relay' entities as BountySystem Events.
+const Relay_BountyRewarded_targetname = "bounty_mode.relay_BountyRewarded";
+const Relay_BountyListingUpdated_targetname = "bounty_mode.relay_BountyListingUpdated";
+
+const relayMessenger = new Base.MessageTask((tag, data) => {
+    if (tag !== BountySystem.MessageTag) return;
+    const { event_name, event_data } = data;
+    if (event_name == "BountyRewarded") {
+        CSS.Msg("BountyRewarded!");
+        CSS.EntFireAtName({
+            name: Relay_BountyRewarded_targetname,
+            input: "Trigger",
+            value: JSON.stringify(event_data), // TODO: serialize CSPlayerPawn, use player slot?
+        });
+    }
+    else if (event_name == "BountyListingUpdated") {
+        CSS.Msg("BountyListingUpdated!");
+        CSS.EntFireAtName({
+            name: Relay_BountyListingUpdated_targetname,
+            input: "Trigger",
+            value: JSON.stringify(event_data),
+        });
+    }
+});
 
 // Go
 Base.Mount.Start();
